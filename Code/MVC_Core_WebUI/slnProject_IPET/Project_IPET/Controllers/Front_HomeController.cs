dc703c9c17a28a3886946155086f784f12c74231
ZI YANG JIANG <straws4857@gmail.com>
2022-04-19 20:32:31 +0800

    上傳 ASP.NET Core MVC 專案。

---
+++ Code/MVC_Core_WebUI/slnProject_IPET/Project_IPET/Controllers/Front_HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_IPET.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_IPET.Controllers
+{
+    public class Front_HomeController : Controller
+    {
+        private readonly ILogger<Front_HomeController> _logger;
+
+        public Front_HomeController(ILogger<Front_HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Code/MVC_Core_WebUI/slnProject_IPET/Project_IPET/ViewModels/CLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_IPET.ViewModels
+{
+    public class CLoginViewModel
+    {
+        public string txtAccount { get; set; }
+        public string txtPassword { get; set; }
+    }
+}

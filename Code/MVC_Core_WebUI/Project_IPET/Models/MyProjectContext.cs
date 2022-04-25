ea687b5bf3c87b941b626e53cadc9b9e9f2569a3
SPOSNNY <sposnny@gmail.com>
2022-04-25 23:51:22 +0800

    新增Entity Framework 模型

---
+++ Code/MVC_Core_WebUI/Project_IPET/Models/MyProjectContext.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Project_IPET.Models
+{
+    public partial class MyProjectContext : DbContext
+    {
+        public MyProjectContext()
+        {
+        }
+
+        public MyProjectContext(DbContextOptions<MyProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<CommentReply> CommentReplies { get; set; }
+        public virtual DbSet<Member> Members { get; set; }
+        public virtual DbSet<MemberRole> MemberRoles { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<PostLiked> PostLikeds { get; set; }
+        public virtual DbSet<PostType> PostTypes { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MyProject;Integrated Security=True");
+//            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Chinese_Taiwan_Stroke_CI_AS");
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.ToTable("Comment");
+
+                entity.Property(e => e.CommentId).HasColumnName("CommentID");
+
+                entity.Property(e => e.BannedContent).IsRequired();
+
+                entity.Property(e => e.CommentContent).HasMaxLength(100);
+
+                entity.Property(e => e.CommentImage).IsUnicode(false);
+
+                entity.Property(e => e.CommentReplyId).HasColumnName("CommentReplyID");
+
+                entity.Property(e => e.OrderDetailId).HasColumnName("OrderDetailID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.HasOne(d => d.CommentReply)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.CommentReplyId)
+                    .HasConstraintName("FK_Comment_CommentReply");
+
+                entity.HasOne(d => d.OrderDetail)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.OrderDetailId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comment_OrderDetails");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Comment_Products");
+            });
+
+            modelBuilder.Entity<CommentReply>(entity =>
+            {
+                entity.ToTable("CommentReply");
+
+                entity.Property(e => e.CommentReplyId).HasColumnName("CommentReplyID");
+            });
+
+            modelBuilder.Entity<Member>(entity =>
+            {
+                entity.Property(e => e.MemberId).HasColumnName("MemberID");
+
+                entity.Property(e => e.Address).HasMaxLength(50);
+
+                entity.Property(e => e.BirthDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.RegionId).HasColumnName("RegionID");
+
+                entity.Property(e => e.RegisteredDate).HasColumnType("datetime");
+
+                entity.Property(e => e.RoleId)
+                    .HasColumnName("RoleID")
+                    .HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.UserId)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("UserID");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.Members)
+                    .HasForeignKey(d => d.RoleId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Members_MemberRoles");
+            });
+
+            modelBuilder.Entity<MemberRole>(entity =>
+            {
+                entity.HasKey(e => e.RoleId)
+                    .HasName("PK_LevelType");
+
+                entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+                entity.Property(e => e.RoleName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.CouponId).HasColumnName("CouponID");
+
+                entity.Property(e => e.DeliveryTypeId).HasColumnName("DeliveryTypeID");
+
+                entity.Property(e => e.Frieght).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.MemberId).HasColumnName("MemberID");
+
+                entity.Property(e => e.OrderName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.OrderPhone)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.OrderStatusId).HasColumnName("OrderStatusID");
+
+                entity.Property(e => e.PayDate).HasMaxLength(50);
+
+                entity.Property(e => e.PaymentTypeId).HasColumnName("PaymentTypeID");
+
+                entity.Property(e => e.RequiredDate).HasMaxLength(50);
+
+                entity.Property(e => e.ShippedDate).HasMaxLength(50);
+
+                entity.Property(e => e.ShippedTo)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.TransactionTypeId).HasColumnName("TransactionTypeID");
+
+                entity.HasOne(d => d.Member)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.MemberId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Orders_Members");
+            });
+
+            modelBuilder.Entity<OrderDetail>(entity =>
+            {
+                entity.Property(e => e.OrderDetailId).HasColumnName("OrderDetailID");
+
+                entity.Property(e => e.OrderId).HasColumnName("OrderID");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrderDetails_Orders");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.OrderDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_OrderDetails_Products");
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.ToTable("Post");
+
+                entity.Property(e => e.PostId).HasColumnName("PostID");
+
+                entity.Property(e => e.BannedContent).IsRequired();
+
+                entity.Property(e => e.MemberId).HasColumnName("MemberID");
+
+                entity.Property(e => e.PostTypeId).HasColumnName("PostTypeID");
+
+                entity.HasOne(d => d.Member)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.MemberId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Post_Members");
+
+                entity.HasOne(d => d.PostType)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.PostTypeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Post_PostType");
+
+                entity.HasOne(d => d.ReplyToPostNavigation)
+                    .WithMany(p => p.InverseReplyToPostNavigation)
+                    .HasForeignKey(d => d.ReplyToPost)
+                    .HasConstraintName("FK_Post_Post");
+            });
+
+            modelBuilder.Entity<PostLiked>(entity =>
+            {
+                entity.ToTable("PostLiked");
+
+                entity.Property(e => e.PostLikedId).HasColumnName("PostLikedID");
+
+                entity.Property(e => e.MemberId).HasColumnName("MemberID");
+
+                entity.Property(e => e.PostId).HasColumnName("PostID");
+
+                entity.HasOne(d => d.Member)
+                    .WithMany(p => p.PostLikeds)
+                    .HasForeignKey(d => d.MemberId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_PostLiked_Members");
+
+                entity.HasOne(d => d.Post)
+                    .WithMany(p => p.PostLikeds)
+                    .HasForeignKey(d => d.PostId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_PostLiked_Post");
+            });
+
+            modelBuilder.Entity<PostType>(entity =>
+            {
+                entity.ToTable("PostType");
+
+                entity.Property(e => e.PostTypeId).HasColumnName("PostTypeID");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.BrandId).HasColumnName("BrandID");
+
+                entity.Property(e => e.CostPrice).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(400);
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.SubCategoryId).HasColumnName("SubCategoryID");
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

4d5668a2be802cc0dd6de519212ca29f81506aba
Powei0412 <91363899+Powei0412@users.noreply.github.com>
2022-04-26 18:04:45 +0800

    前端個人訂單

---
+++ Code/MVC_Core_WebUI/Project_IPET/Controllers/Back_ProjectController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_IPET.Controllers
+{
+    public class Back_ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}

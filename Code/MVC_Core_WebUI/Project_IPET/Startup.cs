12f873940989df36eb0b1a4495b04fa6202f1021
SPOSNNY <sposnny@gmail.com>
2022-04-26 19:24:19 +0800

    no message

--- Code/MVC_Core_WebUI/Project_IPET/Startup.cs
+++ Code/MVC_Core_WebUI/Project_IPET/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;

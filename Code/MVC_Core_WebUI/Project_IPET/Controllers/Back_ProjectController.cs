dfc177cfd3836f0007e90b9b61d38f713bf5c3e7
Powei0412 <91363899+Powei0412@users.noreply.github.com>
2022-05-06 12:02:28 +0800

    Project前後端大概

--- Code/MVC_Core_WebUI/Project_IPET/Controllers/Back_ProjectController.cs
+++ Code/MVC_Core_WebUI/Project_IPET/Controllers/Back_ProjectController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_IPET.Models.EF;
+using Project_IPET.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +10,55 @@ namespace Project_IPET.Controllers
 {
     public class Back_ProjectController : Controller
     {
+        private readonly MyProjectContext _context;
+        public Back_ProjectController(MyProjectContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IQueryable<CProjectViewModel> list = null;
+            list = _context.ProjectDetails.Select(n => new CProjectViewModel
+            {
+                fId = n.PrjId,
+                fTitle = n.Title,
+                fGoal = (int)n.Goal,
+                fStarttime = n.Starttime.ToString(),
+                fEndtime = n.Endtime.ToString(),
+                fFoundation = n.Foundation.FoundationName
+            });
+            return View(list);
         }
 
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        public IActionResult ProjectProduct(int Id)
+        {
+            var productlist = _context.PrjConnects.Where(n => n.PrjId == Id).Select(n=>n.ProductId);
+
+            var list = new List<Product>();
+            foreach (var item in productlist.ToList())
+            {
+                var prod = _context.Products.FirstOrDefault(n => n.ProductId == item);
+                list.Add(prod);
+            }
+            return Json(list);
+        }
+        public IActionResult ProjectDetail(int Id)
+        {
+            var Project = _context.ProjectDetails.Where(n=>n.PrjId == Id).Select(p=>new
+            {
+                detailfoundation = p.Foundation.FoundationName,
+                detailgoal = p.Goal,
+                detailstarttime = p.Starttime.ToString(),
+                detailendtime = p.Endtime.ToString(),
+            });
+            return Json(Project);
         }
     }
 }
---
+++ Code/MVC_Core_WebUI/Project_IPET/ViewModels/CProjectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_IPET.ViewModels
+{
+    public class CProjectViewModel
+    {
+        public int fId { get; set; }
+        public string fTitle { get; set; }
+        public int fGoal { get; set; }
+        public string fFoundation { get; set; }
+        public string fStarttime { get; set; }
+        public string fEndtime { get; set; }
+    }
+}

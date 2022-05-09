765d1eb4271629cc22c143f6c9654a3344fc1fca
PoWeiChen <n8802695@gmail.com>
2022-05-09 19:03:24 +0800

    Front Project Detail

--- Code/MVC_Core_WebUI/Project_IPET/ViewModels/CProjectBuylistViewModel.cs
+++ Code/MVC_Core_WebUI/Project_IPET/ViewModels/CProjectBuylistViewModel.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Project_IPET.ViewModels
 {
-    public class CProjectBuylist
+    public class CProjectBuylistViewModel
     {
         public string UserName { get; set; }
         public string ProductName { get; set; }

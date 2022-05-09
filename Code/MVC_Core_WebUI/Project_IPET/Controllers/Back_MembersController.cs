64dfa410c1e74104fb996491973465589c1cc599
accoutforsharing <dnangel6412@hotmail.com>
2022-05-09 10:50:04 +0800

    +Back_Members >Factory,PartialView +SendEmailService +/EF/Model

--- Code/MVC_Core_WebUI/Project_IPET/Controllers/Back_MembersController.cs
+++ Code/MVC_Core_WebUI/Project_IPET/Controllers/Back_MembersController.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjTest.Models;
+using Project_IPET.Models.EF;
+using Project_IPET.Services;
+using Project_IPET.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +12,35 @@ namespace Project_IPET.Controllers
 {
     public class Back_MembersController : Controller
     {
+        private readonly MyProjectContext _context;
+        public Back_MembersController(MyProjectContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            int countOnePage = 10;
+            //int totalMembers = (new CBackMembersFactory(_context)).memberFilter(txtKeyword,false).Count();
+            int totalMembers = 100;
+            (new CTools()).Page(countOnePage, totalMembers, out int totalPage);
+            ViewBag.PAGE = totalPage;
+
             return View();
+        }
+        [HttpPost]
+        public IActionResult ListView(int inputPage, string txtKeyword)
+        {
+            int page = 1;
+            int countOnePage = 10;
+            if (inputPage > 0)
+                page = inputPage;
+
+            IEnumerable<CBackMembersViewModel> datas = null;
+            datas = (new CBackMembersFactory(_context)).memberFilter(txtKeyword, false)
+                .Skip((page - 1) * countOnePage).Take(countOnePage).ToList();
+
+            return PartialView(datas);
         }
     }
 }
---
+++ Code/MVC_Core_WebUI/Project_IPET/Services/IEmailSenderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Project_IPET.Services
+{
+    public interface IEmailSenderService
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailWithAttachmentsAsync(string email, string subject, string message, List<Attachment> attachments);
+    }
+}

c0c5d89210dea55b94b7a2365f133c12a2329d37
accoutforsharing <dnangel6412@hotmail.com>
2022-05-11 17:02:03 +0800

    +BackMember Ver.1 +Signin, Signup +Front_Myaccount => Content(Edit) => ListView(PartialView) +Model => MembersFactory, MemberFilter +ViewModel => BackMembers, EmptySignup, FrontMembers

--- Code/MVC_Core_WebUI/Project_IPET/Controllers/Empty_SignupController.cs
+++ Code/MVC_Core_WebUI/Project_IPET/Controllers/Empty_SignupController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjTest.Models;
+using Project_IPET.Models.EF;
+using Project_IPET.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,39 @@ namespace Project_IPET.Controllers
 {
     public class Empty_SignupController : Controller
     {
+        private readonly MyProjectContext _context;
+        public Empty_SignupController(MyProjectContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Register(CEmptySignupViewModel vModel)
+        {
+
+            var member = new Member
+            {
+                Name = vModel.LastName + vModel.FirstName,
+                UserId = vModel.UserId,
+                Email = vModel.Email,
+                Gender = (new CMembersFactory(_context)).getGender(vModel.Gender),
+                BirthDate = vModel.BirthDate,
+                Password = vModel.CurrentPwd,
+                Phone = vModel.Phone,
+                RegionId = (new CMembersFactory(_context)).getRegionId(vModel.Region),
+                Address = vModel.Address,
+                RegisteredDate = DateTime.Now,
+            };
+
+            _context.Members.Add(member);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Empty_Signin");
+        }
+
     }
 }

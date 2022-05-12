c58da92b7cbe05f86edceba7e062e1d0390f4c8d
Powei0412 <91363899+Powei0412@users.noreply.github.com>
2022-05-12 13:47:29 +0800

    AddSignalR

---
+++ Code/MVC_Core_WebUI/Project_IPET/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_IPET.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string username, string message, string time = "")
+        {
+            time = DateTime.Now.ToString("HH:mm");
+            if (message == "加入聊天室")
+                //判斷是否是加入聊天室
+                await Clients.All.SendAsync("NewJoin", username, message, time);
+            else if (message == "離開聊天室")
+                //離開聊天
+                await Clients.All.SendAsync("Leave", username, message, time);
+            else
+                //設定所有進入的Client 都收到信息
+                await Clients.All.SendAsync("ReceiveMessage", username, message, time);
+        }
+    }
+}
